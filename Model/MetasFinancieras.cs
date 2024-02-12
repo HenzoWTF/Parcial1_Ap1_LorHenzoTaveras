6a6491160a21dd7a5b3151ff5511f81f4a97cf6b
HenzoWTF <Urenahenzo@gmail.com>
2024-02-12 18:18:45 -0400

    Finalizando el modelo MetasFinanciera

---
+++ Model/MetasFinancieras.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_LorHenzoTaveras.Model
+{
+    public class MetasFinancieras
+    {
+        public int? MetaId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string? Descripcion { get; set; }
+
+        public decimal? Monto { get; set; }
+    }
+}
