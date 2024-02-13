ea6a14774cc50aa0b6b3d86c5031afea11550678
HenzoWTF <Urenahenzo@gmail.com>
2024-02-12 20:17:27 -0400

    Remodelando el model

--- Model/MetasFinancieras.cs
+++ Model/MetasFinancieras.cs
@@ -9,7 +9,11 @@ namespace Parcial1_Ap1_LorHenzoTaveras.Model
 
         [DataType(DataType.Date)]
         public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El campo Descripcion es obligatorio")]
         public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El campo Monto es obligatorio")]
 
         public decimal? Monto { get; set; }
     }
